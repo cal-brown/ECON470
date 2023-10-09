54b1818a30ec92cdd56d01fe67db4199805399fd
Calvin Brown <calvinlongbrown@gmail.com>
2023-10-09 15:19:44 -0700

    undid change to nfv_end, replaced prints with @shows

--- asg1.jl
+++ asg1.jl
@@ -1,26 +1,42 @@
 # Calvin Brown and Jonathan Hagendoorn
 
 using Plots
+using Test
 
 #q1
 function leap_year(year)
 	return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 end
 
+
+@show leap_year(2023) == false
+@show leap_year(2024) == true
+@show leap_year(2100) == false
+@show leap_year(2400) == true
+
 #q2.p1
 function sum_of_squares(n)
 	return reduce(+, map(x -> x^2, 1:n))
 end
+
+@show sum_of_squares(1) == 1
+@show sum_of_squares(5) == 55
 
 #q2.p2
 function square_of_sums(n)
 	return reduce(+, 1:n)^2
 end
 
+@show square_of_sums(1) == 1
+@show square_of_sums(5) == 225
+
 #q2.p3
 function difference_in_squares(n)
 	return sum_of_squares(n) - square_of_sums(n)
 end
+
+@show difference_in_squares(1) == 0 
+@show difference_in_squares(5) == -170
 
 #q3
 function NPV(c, r) 
@@ -31,8 +47,10 @@ function NPV(c, r)
 	return sum
 end
 
+@show NPV([1000, 1100, 1210], .10) ≈ 3000 
+
 #q4.p1
-function NFV_end(c, r)
+function NFV_end(c, r::Real)
 	sum = 0
 	n = length(c)
 	for i in eachindex(c)
@@ -41,14 +59,20 @@ function NFV_end(c, r)
 	return sum
 end
 
+@show NFV_end([1000, 1000, 1000], .10) ≈ 1100 + 1210 + 1331
+
 #q4.p2
-function NFV_end_2(c, r)
+function NFV_end(c, r::Array)
 	sum = 0
 	for i in eachindex(c)
-		sum += c[i]*(1+r[i])
+		sum += c[i]
+		sum *= 1+r[i]
 	end
 	return sum
 end
+
+@show NFV_end([1000, 1000, 1000], [.10, .10, .10]) ≈ NFV_end([1000, 1000, 1000], .10)
+@show NFV_end([1000, 1000, 1000], [.20, .10, .05]) ≈ 3591 
 
 #q5.p1
 function expense(x)
@@ -61,12 +85,18 @@ function expense(x)
 	return x*.6
 end
 
+@show expense(20) ≈ 20 * 0.8
+@show expense(200) ≈ 200 * 0.7
+@show expense(500) ≈ 500 * 0.6
+
 #q5.p2
 function plot_expense()
 	x = range(0, 300)
 	y = expense.(x)
 	plot(x, y)
 end
+
+@show plot_expense()
 
 #q6.p1
 function salary(w, L)
@@ -86,6 +116,12 @@ function salary(w, L)
 	end
 end
 
+@show salary(-1, 0) == "please enter a non-negative hourly wage" 
+@show salary(1, -1) == "please enter a non-negative number of hours worked"
+@show salary(1, 13) == "please enter a number of hours worked no greater than 12"
+@show salary(18, 6) ≈ 6 * 18
+@show salary(17, 10) ≈ 8 * 17 + 2.0 * 17 * 1.5
+
 function plot_salary()
 	x = range(1, 12, length=12)
 	y = salary.(20,x)
@@ -93,11 +129,5 @@ function plot_salary()
 end
 
 # some very quick tests
-println("Leap years (2000, 2100, 234123, 4328): $(leap_year(2000)), $(leap_year(2100)), $(leap_year(234123)), $(leap_year(4328))")
-println("Sum of squares (n=5): $(sum_of_squares(5))")
-println("Square of sums (n=5): $(square_of_sums(5))")
-println("Diff in squares (n=5): $(difference_in_squares(5))")
-
-println("NFV_end_2 of c = (100, 100, 200, 100), r = (.10, .20, .10, .05): ", NFV_end_2([100, 100, 200, 100], [.10, .20, .10, .05])) 
-plot_expense()
-plot_salary()
+#plot_expense()
+@show plot_salary()
